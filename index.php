e6174040fef18574eb91a066ed824b4a8c8ddca6
Cedric Lima <limacedric@hotmail.fr>
2021-03-16 13:54:38 +0100

    Revision jquery

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="jquery.js"></script>
+    <script src="script.js"></script>
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+   <p id="p1" style="background-color:red;">Lorem ipsum dolor sit amet. 1</p>
+   <p id="p2" style="background-color:blue;">Lorem ipsum dolor sit amet. 2</p>
+   <p id="p3" style="background-color:green;">Lorem ipsum dolor sit amet. 3</p>
+   <p id="p4" style="background-color:yellow;">Lorem ipsum dolor sit amet. 4</p>
+   <p id="p5" style="background-color:brown;">Lorem ipsum dolor sit amet. 5</p>
+
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ script.js
@@ -0,0 +1,10 @@
+$(function(){
+
+   $("#p1").on('click',function(){
+      $('#p5').hide()
+   });
+
+   $("#p1").on('mouseleave',function(){
+      $('#p5').show()
+   });
+})
