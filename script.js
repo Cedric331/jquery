3bc744113cb66445bd70f98fb0e7b241ec32fcae
Cedric Lima <limacedric@hotmail.fr>
2021-03-16 14:03:17 +0100

    Zffet Jquery

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 $(function(){
 
-   $("#p1").on('click',function(){
-      $('#p5').hide(1000, function(){
-         $('#p4').hide(1000, function(){
-            $('#p3').hide(1000, function(){
-               $('#p2').hide(1000)
-            })
-         })
-      })
-   });
-})
+   // Function callback
+
+   // $("#p1").on('click',function(){
+   //    $('#p5').hide(1000, function(){
+   //       $('#p4').hide(1000, function(){
+   //          $('#p3').hide(1000, function(){
+   //             $('#p2').hide(1000)
+   //          })
+   //       })
+   //    })
+   // });
+
+   // Toggle
+
+   // $("#p1").on('click',function(){
+   //    $('#p5').toggle();
+   //    });
+
+   // Fade
+   // $("#p1").on('click',function(){
+   //    $('#p5').fadeOut(3000);
+   //    });
+
+      // $("#p1").on('click',function(){
+      //    $('#p5').fadeIn(3000);
+      // });
+
+      // $("#p1").on('click',function(){
+      //    $('#p5').fadeToggle(3000);
+      // });
+});
